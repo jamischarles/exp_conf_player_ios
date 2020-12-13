b1687fe47b14f486482cfdff8c17bad2f28775ce
Jamis Charles <jacharles@paypal.com>
2020-12-13 07:26:27 -0700

    Initial Commit

---
+++ Music Player Exp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Music Player Exp
+//
+//  Created by Jamis Charles on 12/13/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Music Player Exp/Music_Player_ExpApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_Player_ExpApp.swift
+//  Music Player Exp
+//
+//  Created by Jamis Charles on 12/13/20.
+//
+
+import SwiftUI
+
+@main
+struct Music_Player_ExpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
